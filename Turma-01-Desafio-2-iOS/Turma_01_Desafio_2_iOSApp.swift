30ca9b565a63823d7382a62237b6d8e97dff3ed2
Pedro Galdiano <pedrocastro014@gmail.com>
2021-12-15 11:44:13 -0300

    Criando o projeto

---
+++ Turma-01-Desafio-2-iOS/Turma_01_Desafio_2_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Turma_01_Desafio_2_iOSApp.swift
+//  Turma-01-Desafio-2-iOS
+//
+//  Created by PEDRO GALDIANO DE CASTRO on 15/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Turma_01_Desafio_2_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
